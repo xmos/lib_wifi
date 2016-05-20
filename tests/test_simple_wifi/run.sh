15d3b8a43489854955edd6e9d087d85903d559b3
Sam Chesney <samc@xmos.com>
2016-05-20 14:39:26 +0100

    Quick tidy up of run script

--- tests/test_simple_wifi/run.sh
+++ tests/test_simple_wifi/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-xgdb bin/test_simple_wifi.xe -ex="connect --xscope-port 127.0.0.1:10234" -ex="load" \
+xgdb bin/test_simple_wifi.xe \
+ -ex="connect --xscope-port 127.0.0.1:10234" \
+ -ex="load" \
  -ex="set args \"$1\" \"$2\"" \
  -ex="c"
